99708ad68a1b1b345733fe184966ead8c2b6949e
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-09-26 00:54:12 -0500

    fixed

---
+++ scripts/deploy_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+services=(
+    "doctor_payout_audit_log_service,dp-AuditLogService7268,DoctorPayout-AuditLogService"
+    "doctor_payout_auth_service,dp-AuthService7261,DoctorPayout-AuthService"
+    "doctor_payout_component_service,dp-ComponentService7269,DoctorPayout-ComponentService"
+    "doctor_payout_doctor_service,dp-DoctorService7272,DoctorPayout-DoctorService"
+    "doctor_payout_entitlement_service,dp-EntitlementService7262,DoctorPayout-EntitlementService"
+    "doctor_payout_hospital_service,dp-HospitalService7263,DoctorPayout-HospitalService"
+    "doctor_payout_master_service,dp-MasterService7264,DoctorPayout-MasterService"
+    "doctor_payout_parameter_service,dp-ParameterService7266,DoctorPayout-ParameterService"
+    "doctor_payout_payout_service,dp-PayoutService7273,DoctorPayout-PayoutService"
+    "doctor_payout_rule_engine_service,dp-RuleEngineService7275,DoctorPayout-RuleEngineService"
+    "doctor_payout_service_discovery_service,dp-ServiceDiscoveryService7270,DoctorPayout-ServiceDiscoveryService"
+    "doctor_payout_storage_service,dp-StorageService7125,DoctorPayout-StorageService"
+    "doctor_payout_user_service,dp-UserService7265,DoctorPayout-UserService"
+    "doctor_payout_variable_service,dp-VariableService7267,DoctorPayout-VariableService"
+    "doctor_payout_workflow_service,dp-WorkflowService7274,DoctorPayout-WorkflowService"
+)
+
+for service in "${services[@]}"; do
+    IFS=',' read -r SERVICE_NAME ZIP_DIR SERVICE_DIR <<< "$service"
+    ./deploy_base.sh "$SERVICE_NAME" "$ZIP_DIR" "$SERVICE_DIR"
+done
--- scripts/deploy_audit_log_service.sh
+++ scripts/deploy_audit_log_service.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-rm -rf /opt/local/share/deploy/temp
-mkdir -p /opt/local/share/deploy/temp
-
-
-BASE_PATH="/opt/local/share"
 SERVICE_NAME="doctor_payout_audit_log_service"
 ZIP_DIR="dp-AuditLogService7268"
-ZIP_FILE_NAME="$ZIP_DIR.zip"
-EXTRACT_DIR="/opt/local/share/deploy/temp"
-LOCAL_ZIP_PATH="$BASE_PATH/deploy/$ZIP_FILE_NAME"
 SERVICE_DIR="DoctorPayout-AuditLogService"
 
-# Stop the service
-echo "Stopping service: $SERVICE_NAME"
-systemctl stop "$SERVICE_NAME"
-
-# Extract the zip file
-echo "Extracting zip file to: $EXTRACT_DIR"
-unzip -o "$LOCAL_ZIP_PATH" -d "$EXTRACT_DIR"
-
-# Extract the zip file
-echo "Copying contents to: $EXTRACT_DIR"
-rm -rf "$BASE_PATH/$SERVICE_DIR/publish"
-mkdir -p "$BASE_PATH/$SERVICE_DIR/publish"
-cp -r $EXTRACT_DIR/$ZIP_DIR/* $BASE_PATH/$SERVICE_DIR/publish -v
-
-rm -rf /opt/local/share/deploy/temp
-
-# Start the service
-echo "Starting service: $SERVICE_NAME"
-systemctl start "$SERVICE_NAME"
+./deploy_base.sh "$SERVICE_NAME" "$ZIP_DIR" "$SERVICE_DIR"
