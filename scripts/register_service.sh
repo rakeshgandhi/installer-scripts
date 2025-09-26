7200b42a0e3a8431f5675c385c59c0924ab177f4
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-09-25 22:13:12 -0500

    fixes

--- scripts/register_service.sh
+++ scripts/register_service.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 services=(
-    "doctor_payout_audit_log_service",
-    "doctor_payout_auth_service",
-    "doctor_payout_component_service",
-    "doctor_payout_doctor_service",
-    "doctor_payout_entitlement_service",
-    "doctor_payout_hospital_service",
-    "doctor_payout_master_service",
-    "doctor_payout_parameter_service",
-    "doctor_payout_payout_service",
-    "doctor_payout_rule_engine_service",
-    "doctor_payout_service_discovery_service",
-    "doctor_payout_storage_service",
-    "doctor_payout_user_service",
-    "doctor_payout_variable_service",
+    "doctor_payout_audit_log_service"
+    "doctor_payout_auth_service"
+    "doctor_payout_component_service"
+    "doctor_payout_doctor_service"
+    "doctor_payout_entitlement_service"
+    "doctor_payout_hospital_service"
+    "doctor_payout_master_service"
+    "doctor_payout_parameter_service"
+    "doctor_payout_payout_service"
+    "doctor_payout_rule_engine_service"
+    "doctor_payout_service_discovery_service"
+    "doctor_payout_storage_service"
+    "doctor_payout_user_service"
+    "doctor_payout_variable_service"
     "doctor_payout_workflow_service"
 )
 
