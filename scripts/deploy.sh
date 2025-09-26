e14a8ecfc3aa38b27fa2409f133cb969fc80800a
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-09-25 23:28:20 -0500

    added script to deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir -p /opt/local/share/deploy/temp
+
+BASE_PATH="/opt/local/share/deploy"
+SERVICE_NAME="doctor_payout_audit_log_service"
+ZIP_DIR="dp-AuditLogService7268"
+ZIP_FILE_NAME="$ZIP_DIR.zip"
+EXTRACT_DIR="/opt/local/share/deploy/temp"
+LOCAL_ZIP_PATH="$EXTRACT_DIR/$ZIP_FILE_NAME"
+SERVICE_DIR="DoctorPayout-AuthService"
+
+# Stop the service
+echo "Stopping service: $SERVICE_NAME"
+systemctl stop "$SERVICE_NAME"
+
+# Extract the zip file
+echo "Extracting zip file to: $EXTRACT_DIR"
+unzip -o "$LOCAL_ZIP_PATH" -d "$EXTRACT_DIR"
+
+# Extract the zip file
+echo "Copying contents to: $EXTRACT_DIR"
+rm -rf "$BASE_PATH/$SERVICE_DIR/publish"
+mkdir -p "$BASE_PATH/$SERVICE_DIR/publish"
+cp -r "$EXTRACT_DIR/$ZIP_DIR"* "$BASE_PATH/$SERVICE_DIR/publish"
+
+# Start the service
+echo "Starting service: $SERVICE_NAME"
+systemctl start "$SERVICE_NAME"
