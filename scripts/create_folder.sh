501908c033cb95d89dcd5f933e771a9227c6b7cf
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-09-25 21:55:58 -0500

    corrected folder name

--- scripts/create_folder.sh
+++ scripts/create_folder.sh
@@ -2,21 +2,21 @@
 
 folder_base_path="/opt/local/share"
 folders=(
-    "DoctorPayout-AuditLogService",
-    "DoctorPayout-AuthService",
-    "DoctorPayout-ComponentService",
-    "DoctorPayout-DoctorService",
-    "DoctorPayout-EntitlementService",
-    "DoctorPayout-HospitalService",
-    "DoctorPayout-MasterService",
-    "DoctorPayout-ParameterService",
-    "DoctorPayout-PayoutService",
-    "DoctorPayout-RuleEngineService",
-    "DoctorPayout-ServiceDiscoveryService",
-    "DoctorPayout-StorageService",
-    "DoctorPayout-UserService",
-    "DoctorPayout-VariableService",
-    "DoctorPayout-WorkflowService",
+    "DoctorPayout-AuditLogService"
+    "DoctorPayout-AuthService"
+    "DoctorPayout-ComponentService"
+    "DoctorPayout-DoctorService"
+    "DoctorPayout-EntitlementService"
+    "DoctorPayout-HospitalService"
+    "DoctorPayout-MasterService"
+    "DoctorPayout-ParameterService"
+    "DoctorPayout-PayoutService"
+    "DoctorPayout-RuleEngineService"
+    "DoctorPayout-ServiceDiscoveryService"
+    "DoctorPayout-StorageService"
+    "DoctorPayout-UserService"
+    "DoctorPayout-VariableService"
+    "DoctorPayout-WorkflowService"
 )
 
 for folder in "${folders[@]}"; do
