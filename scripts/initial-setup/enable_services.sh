452bd800edf10de948be5f123df0c242a36aa5dc
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-11-11 22:30:55 -0600

    added services in enable and start

--- scripts/initial-setup/enable_services.sh
+++ scripts/initial-setup/enable_services.sh
@@ -17,7 +17,8 @@ services=(
     "doctor_payout_user_service"
     "doctor_payout_variable_service"
     "doctor_payout_workflow_service"
-)
+    "doctor_payout_loader_worker_service"
+    "doctor_payout_rule_engine_worker_service"
 
 for service in "${services[@]}"; do
     echo "Enabling service: $service"
