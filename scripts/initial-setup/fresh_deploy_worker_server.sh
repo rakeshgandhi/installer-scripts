387d28ecbe0f212926f0ca318fe2da0be672723e
Gandhi, Rakesh <rakesh.gandhi@geminisolutions.com>
2025-11-23 00:38:13 -0600

    fixed issue

--- scripts/initial-setup/fresh_deploy_worker_server.sh
+++ scripts/initial-setup/fresh_deploy_worker_server.sh
@@ -4,5 +4,5 @@
 ./create_worker_services_app_directories.sh
 ./create_worker_services_service_config.sh
 ./enable_worker_services.sh
-./start_worker_services.sh
+./start_services_worker_server.sh
 ./create_deploy_directories.sh
